412181284e14811b8b2a31bedc384f3635b7f410
José Cruz Chávez <josecruzchavez10@gmail.com>
2024-01-17 02:49:55 -0600

    Fix to open only parent content

--- view/frontend/web/js/gdw_faqs_accordion.js
+++ view/frontend/web/js/gdw_faqs_accordion.js
@@ -1,8 +1,8 @@
 require(['jquery'], function($){
     $('.question-title').on('click', function(){
         var _this = $(this);
-        var _parent = _this.parents('.question-body');
-        var _answer = _parent.find('.question-answer');
+        var _parent = _this.parent();
+        var _answer = _parent.children('.question-answer');
         if(!_parent.hasClass('active')){
             _this.addClass('active');
             _parent.addClass('active');
